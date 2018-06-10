baea1138dcc9dadfb0956767ac83ce7a6080ac5d
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-06-10 15:54:43 +0200

    add Update todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParcer = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParcer = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require ('./db/mongoose');
@@ -59,8 +60,32 @@ app.delete('/todos/:id', (req,res) => {
             return res.status(200).send({todo})
     }).catch((e) => {
         res.status(400).send()
-});
     });
+});
+
+app.patch('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    var body =_.pick(req.body, ['text','completed'])
+
+        if(!ObjectID.isValid(id)){
+            return res.status(404).send();
+}
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set:body},{new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+                    res.send({todo});
+    }).catch((e) => {
+        res.status(400).send()
+})
+
+})
 
 app.listen(port,() => {
     console.log(`Server up on port ${port}`);
