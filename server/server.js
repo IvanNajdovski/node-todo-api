a1139bbe5e2ce16daac6394e1292e00d72e41b98
Ivan Najdovski <ivan_najdovski@hotmail.com>
2018-06-13 14:06:42 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ app.post('/todos', (req,res) => {
         res.status(400).send(e);
 });
 });
-console.log("ivan");
 app.get('/todos', (req,res) => {
    Todo.find().then((todos) => {
        res.send({
@@ -119,6 +118,14 @@ User.findByCredentials(body.email,body.password).then((user) => {
 });
 }).catch((e) => {
     res.status(400).send();
+});
+});
+
+app.delete('/users/me/token', authenticate, (req,res ) => {
+   req.user.removeToken(req.token).then(() => {
+       res.status(200).send()
+}, () => {
+        res.status(400).send();
 });
 });
 
